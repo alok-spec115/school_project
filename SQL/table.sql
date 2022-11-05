f35fc18a6800aac3c9ce27b1e79b527cd2281cc8
alok-spec115 <80452690+alok-spec115@users.noreply.github.com>
2022-11-05 18:48:10 +0530

    Add files via upload

---
+++ SQL/table.sql
@@ -0,0 +1,34 @@
+create database examination;
+use examination;
+
+
+create table unit_test1 (
+	name_ varchar(20),
+    maths int,
+    physics int,
+    chemistry int,
+    english int,
+    computer int,
+    optional_ int
+);
+
+
+create table unit_test2 (
+	name_ varchar(20),
+    maths int,
+    physics int,
+    chemistry int,
+    english int,
+    computer int,
+    optional_ int
+);
+
+create table unit_test4 (
+	name_ varchar(20),
+    maths int,
+    physics int,
+    chemistry int,
+    english int,
+    computer int,
+    optional_ int
+);
