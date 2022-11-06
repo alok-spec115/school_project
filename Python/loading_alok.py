7b3064d7069bfe71a992611340529e92404e7adc
alok-spec115 <80452690+alok-spec115@users.noreply.github.com>
2022-11-06 17:20:30 +0530

    Update loading_alok.py

--- Python/loading_alok.py
+++ Python/loading_alok.py
@@ -7,9 +7,9 @@ for x in range(3):
        time.sleep(1)
        print('.', end='')
 time.sleep(1)
-
 print()
+
 
 for x in range(29):
        print(chr(9606), end='') # or chr(9617)
-       time.sleep( random.choice([0, 0.1, 0.2, 0.3]) )
+       time.sleep( random.choice([0.1, 0.3, 0.5, 1, 1.5]) )
