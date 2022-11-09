34c8beb7e35d27f86cac2440cf9f98bb1947c597
alok-spec115 <80452690+alok-spec115@users.noreply.github.com>
2022-11-09 21:17:53 +0530

    Update harshit_test.py

--- Python/harshit_test.py
+++ Python/harshit_test.py
@@ -1,50 +1,21 @@
-# Python3 implementation of the approach 
+from time import*
+a = """
 
-  
-# Function to print the doormat pattern 
+░█─ ░█ ░█▀▀▀  ░█──  ░█▀▀█ ░█▀▀▀█ ░█▀▄▀█ ░█▀▀▀ 
+░█░█░█ ░█▀▀▀  ░█─── ░█─── ░█──   ░█░█░█ ░█ █▀▀▀ 
+░█▄▀▄█ ░█▄▄▄  ░█▄▄█ ░█▄▄█ ░█▄▄▄█ ░█──░█ ░█▄▄▄
 
-def doormatPattern(rows, columns): 
+"""
 
-    width = columns 
+print(a)
+sleep(2)
 
-      
+b = """
 
-    # Print the pattern having a top triangle 
+▄▀█   █▀█ █▀█ █▀█ ░░█ █▀▀ █▀▀ ▀█▀   █▀█ █▄░█
+█▀█   █▀▀ █▀▄ █▄█ █▄█ ██▄ █▄▄ ░█░   █▄█ █░▀█
+"""
+print(b)
+sleep(2)
 
-    for i in range (0, int (rows / 2)): 
-
-        pattern = "|*|" * ((2 * i) + 1) 
-
-        print (pattern.center (width, '-')) 
-
-      
-
-    # Print GeeksforGeeks in the center 
-
-    print ("W E L C O M E".center (width, '-')) 
-
-      
-
-    # Print the pattern having  
-
-    # an inverted triangle 
-
-    i = int (rows / 2) 
-
-    while i > 0: 
-
-        pattern = "|*|" * ((2 * i) - 1) 
-
-        print (pattern.center (width, '-')) 
-
-        i = i-1
-
-    return
-
-  
-# Driver code 
-
-rows = 20
-
-columns = rows * 3
-doormatPattern(rows, columns) 
+print("MEMBERS:\n")
