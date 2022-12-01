55bf61748e1feeeadafca244921b7bab92e4e686
alok-spec115 <80452690+alok-spec115@users.noreply.github.com>
2022-12-01 20:15:44 +0530

    Update and rename loading_alok.py to welcome_message.py

---
+++ Python/welcome_message.py
@@ -0,0 +1,54 @@
+from time import*
+import random
+a = """
+                                       █─            █     █▀▀▀      █──               █▀▀█      █▀▀▀█     █▀▄▀█      █▀▀▀ 
+                                       █     █     █     █▀▀▀      █───            █───             █──          █     █    █      █      █▀▀▀ 
+                                       █▄▀▄█     █▄▄▄      █▄▄█     █▄▄█      █▄▄▄█     █──          █      █▄▄▄
+"""
+
+print(a)
+sleep(2)
+
+b = """
+
+                     █▀▀█ 　      █▀▀█ █▀▀█ █▀▀█           ▀ █▀▀ █▀▀ ▀▀█▀▀ 　 █▀▀█      █▄          █ 
+                     █▄▄█ 　      █▄▄█ █▄▄▀ █          █          █ █▀▀ █                     █                █          █      █     █     █ 
+                     █          █ 　      █               ▀    ▀▀ ▀▀▀▀ █▄█ ▀▀▀ ▀▀▀            █          　 ▀▀▀▀      █       ▀█
+"""
+print(b)
+sleep(1)
+
+c="""
+                            █▀▀ ▀▄▀ ▄▀█    █▀▄▀█ █ █▄    █ ▄▀█ ▀█▀ █ █▀█ █▄    █
+                            ██▄ █    █ █▀█ █    ▀      █ █ █    ▀█ █▀█      █     █ █▄█ █    ▀█
+
+                            █▀▄▀█ ▄▀█ █▄    █ ▄▀█ █▀▀ █▀▀ █▀▄▀█ █▀▀ █▄    █ ▀█▀
+                            █    ▀      █ █▀█ █    ▀█ █▀█ █▄█ ██▄ █    ▀     █ ██▄ █    ▀█      █    
+
+"""
+print(c)
+sleep(2)
+
+print("Members -->")
+sleep(1)
+print("                  ⊛ Harshit")
+sleep(1)
+print("                  ⊛ Priyanshu")
+sleep(1)
+print("                  ⊛ Ansh")
+sleep(1)
+print("                  ⊛ Alok")
+
+sleep(1)
+print('\nLoading', end='')
+for x in range(3):
+       sleep(1)
+       print('.', end='')
+sleep(1)
+print()
+
+
+for x in range(29):
+       print(chr(9606), end='') # or chr(9617)
+       sleep( random.choice([0.1, 0.13, 0.01, 0.09]) )
+
